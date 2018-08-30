c6187d3e6c34b95dbbc2ba074a8489be3232b5d1
pradeepanvi <pradeepkumar.anvi@gmail.com>
2018-08-30 17:16:18 +0530

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -109,6 +109,17 @@ UserSchema.statics.findByCredentials = function(email, password){
     })
 }
 
+// User Log Out
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+
+    return user.update({
+        $pull: {
+            tokens: {token}
+        }
+    })
+}
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User}
